2232ab2f6a6b6a72eb24cc7df0ef82dc13a30042
gyp719 <836230540@qq.com>
2022-11-28 12:15:54 +0900

    API 基础配置

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,8 @@ class Kernel extends HttpKernel
         // API 中间件组，应用于 routes/api.php 路由文件，
         // 在 RouteServiceProvider 中设定
         'api' => [
+            // Header 头
+            \App\Http\Middleware\AcceptHeader::class,
             // 使用别名来调用中间件
             // 请见：https://learnku.com/docs/laravel/9.x/middleware#为路由分配中间件
             'throttle:api',
