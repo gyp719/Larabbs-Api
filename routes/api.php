b5108214450a3e7366827703c51d9782e9be0084
gyp719 <836230540@qq.com>
2022-11-28 21:38:03 +0900

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\VerificationCodesController;
 use App\Http\Controllers\Api\UsersController;
 
-Route::prefix('v1')->name('api.v1.')->group(function() {
-    // 短信验证码
-    Route::post('verificationCodes', [VerificationCodesController::class, 'store'])->name('verificationCodes.store');
-    // 用户注册
-    Route::post('users', [UsersController::class, 'store'])->name('users.store');
+Route::prefix('v1')->name('api.v1.')->group(function () {
+    Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function () {
+            // 短信验证码
+            Route::post('verificationCodes', [VerificationCodesController::class, 'store'])->name('verificationCodes.store');
+            // 用户注册
+            Route::post('users', [UsersController::class, 'store'])->name('users.store');
+        });
+
 });
