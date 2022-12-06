03f80708c5876488fb7470af6e6315b2af032b1b
gyp719 <836230540@qq.com>
2022-12-06 15:37:34 +0800

    话题详情

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Queries\TopicQuery;
 use App\Http\Requests\Api\TopicRequest;
 use App\Http\Resources\TopicResource;
 use App\Models\Topic;
@@ -12,32 +13,16 @@ use Spatie\QueryBuilder\QueryBuilder;
 
 class TopicsController extends Controller
 {
-    public function index(Request $request, Topic $topic)
+    public function index(Request $request, TopicQuery $query)
     {
-        $topics = QueryBuilder::for(Topic::class)
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',
-                AllowedFilter::exact('category_id'),
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
+        $topics = $query->paginate();
 
         return TopicResource::collection($topics);
     }
 
-    public function userIndex(Request $request, User $user)
+    public function userIndex(Request $request, User $user, TopicQuery $query)
     {
-        $query = $user->topics()->getQuery();
-
-        $topics = QueryBuilder::for($query)
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',
-                AllowedFilter::exact('category_id'),
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
+        $topics = $query->where('user_id', $user->id)->paginate();
 
         return TopicResource::collection($topics);
     }
@@ -47,6 +32,13 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = $request->user()->id;
         $topic->save();
+
+        return new TopicResource($topic);
+    }
+
+    public function show($topic_id, TopicQuery $query)
+    {
+        $topic = $query->findOrFail($topic_id);
 
         return new TopicResource($topic);
     }
